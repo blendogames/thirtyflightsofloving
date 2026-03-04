41415fe57a4f57afa91e3ddeecd1e933e21d668c
Ethan Lee <flibitijibibo@gmail.com>
2026-03-04 15:34:02 -0500

    ci: A bunch of fixes for the Mac build environment

--- fixupMac.sh
+++ fixupMac.sh
@@ -2,11 +2,17 @@
 
 # flibit has this solely for convenience when cleaning up Mac binaries.
 
-cd "`dirname "$0"`"/quake2
+cd "`dirname "$0"`"/tfol
 mv kmquake2 tfol
 install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @rpath/libSDL2-2.0.0.dylib tfol
 install_name_tool -change libpng16.16.dylib @rpath/libpng16.16.dylib tfol
 install_name_tool -change /usr/local/lib/libjpeg.9.dylib @rpath/libjpeg.9.dylib tfol
 install_name_tool -change /usr/local/lib/libvorbisfile.3.dylib @rpath/libvorbisfile.3.dylib tfol
+install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @rpath/libSDL2-2.0.0.dylib baseq2/kmq2gamex64.dylib
+install_name_tool -change libpng16.16.dylib @rpath/libpng16.16.dylib baseq2/kmq2gamex64.dylib
+install_name_tool -change /usr/local/lib/libjpeg.9.dylib @rpath/libjpeg.9.dylib baseq2/kmq2gamex64.dylib
+install_name_tool -change /usr/local/lib/libvorbisfile.3.dylib @rpath/libvorbisfile.3.dylib baseq2/kmq2gamex64.dylib
 strip -S tfol
+strip -S baseq2/kmq2gamex64.dylib
 otool -L tfol
+otool -L baseq2/kmq2gamex64.dylib
